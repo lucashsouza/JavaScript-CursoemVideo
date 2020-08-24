7e2e3b4ac18963de0e4485c2afea70e99b7269fb
Lucas Souza <lhenriquesouza00@gmail.com>
2020-08-24 00:47:24 -0300

    Repetições (Parte 1) - Curso JavaScript #13

---
+++ aula13/ambiente.js
@@ -0,0 +1,12 @@
+var teste1 = 1;
+var teste2 = 1;
+
+while (teste1 <= 100) {
+    console.log(`Passo -> ${teste1}`);
+    teste1++;
+}
+
+do {
+    console.log(`Passo -> ${teste2}`);
+    teste2++;
+} while (teste2 <= 100);
