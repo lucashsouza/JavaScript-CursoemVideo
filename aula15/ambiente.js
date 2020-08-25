9d63887463a961d64a300e14bf6943b46c1848e7
Lucas Souza <lhenriquesouza00@gmail.com>
2020-08-25 19:50:49 -0300

    Variáveis Compostas - Curso JavaScript #15

---
+++ aula15/ambiente.js
@@ -0,0 +1,12 @@
+let num = [5, 8, 4];
+
+// Add na posicao 3
+num[3] = 6;
+
+// Add na ultima posicao
+num.push(7)
+
+// Quantidade de elementos 
+console.log(`Quantidade: ${num.length}`)
+
+console.log(`Vetor [${num}]`);
